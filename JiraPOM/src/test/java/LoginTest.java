e92815fb516896000424c3ca5c996634d2c68785
Koaru <mucsanyi.b@gmail.com>
2023-01-02 11:26:20 +0100

    first few testcase for login

--- JiraPOM/src/test/java/LoginTest.java
+++ JiraPOM/src/test/java/LoginTest.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import pageFactory.*;
 
 public class LoginTest {
-    WebDriver driver;
-    Login login;
-    final String URL = "https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa";
+    static WebDriver driver;
+    static Login login;
+    static final String URL = "https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa";
+    final String VALID_USERNAME = System.getenv("VALID_USERNAME");
+    final String VALID_PASSWORD = System.getenv("VALID_PASSWORD");
+    final String INVALID_USERNAME = "invalid-username";
+    final String INVALID_PASSWORD = "invalid-password";
 
     @BeforeAll
-    public void setup(){
+    public static void setup(){
         driver = new ChromeDriver();
         login = new Login(driver);
         driver.get(URL);
@@ -19,6 +23,22 @@ public class LoginTest {
 
     @Test
     public void validLogin(){
+        login.setUsername(VALID_USERNAME);
+        login.setPassword(VALID_PASSWORD);
+        login.clickOnLoginBtn();
+    }
 
+    @Test
+    public void invalidLoginWithInvalidUsername(){
+        login.setUsername(INVALID_USERNAME);
+        login.setPassword(VALID_PASSWORD);
+        login.clickOnLoginBtn();
+    }
+
+    @Test
+    public void invalidLoginWithInvalidPassword(){
+        login.setUsername(VALID_USERNAME);
+        login.setPassword(INVALID_PASSWORD);
+        login.clickOnLoginBtn();
     }
 }
